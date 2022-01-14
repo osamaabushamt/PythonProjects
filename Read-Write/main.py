c12a91c4743e87e3d34f8208f51acf128ab4cdb3
osamaabushamt <osamaabushamt@gmail.com>
2022-01-14 17:07:11 -0600

    5

---
+++ Read-Write/main.py
@@ -0,0 +1,7 @@
+#with open("C:\Users\Owner\Desktop\my_file.txt", mode="w") as file:
+#    file.write("\nOsama is King")
+#
+
+with open("/././my_file.txt") as file:
+    contents = file.read()
+    print(contents)
